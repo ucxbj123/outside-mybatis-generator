17bc42a4b7b26b0f0fbf5a55c8b336a4c1e7b55a
xie <xie@qq.com>
2022-07-18 16:56:17 +0800

    commit mybatis-generator sqlserver MyBatis3Simple QBC

--- src/test/java/org/example/SelectTest.java
+++ src/test/java/org/example/SelectTest.java
@@ -1,42 +1,43 @@
 package org.example;
 
+
 import SqlServer.Dao.Computer;
-import SqlServer.mapper.SelectSQLServerMapper;
-import com.github.pagehelper.Page;
+import SqlServer.Dao.ComputerExample;
+import SqlServer.mapper.ComputerMapper;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.example.Dao.Company;
 import org.example.Mapper.SelectMapper;
 import org.example.Unit.SqlSessionUnit;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
-
-import org.apache.ibatis.io.Resources;
 
 public class SelectTest {
 
-    @Test
-    public void testSelect() throws IOException {
-        SqlSession sqlSession=new SqlSessionUnit("mybatis-config.xml").getSqlSeesion();
-        SelectMapper mapper=sqlSession.getMapper(SelectMapper.class);
-        List<Company> list=mapper.selectCompanyPageInterceptor(50);
-        for (Company company:list){
-            System.out.println(company);
-        }
-    }
-
+//    @Test
+//    public void testSelect() throws IOException {
+//        SqlSession sqlSession=new SqlSessionUnit("mybatis-config.xml").getSqlSeesion();
+//        SelectMapper mapper=sqlSession.getMapper(SelectMapper.class);
+//        List<Company> list=mapper.selectCompanyPageInterceptor(50);
+//        for (Company company:list){
+//            System.out.println(company);
+//        }
+//    }
+//
     @Test
     public void testgetAll() throws IOException {
         SqlSession sqlSession=new SqlSessionUnit("mybatis-config.xml").getSqlSeesion();
-        SelectSQLServerMapper mapper=sqlSession.getMapper(SelectSQLServerMapper.class);
-        PageHelper.startPage(5,6);
-        List<Computer> list=mapper.getAll();
+        ComputerMapper mapper=sqlSession.getMapper(ComputerMapper.class);
+        //QBC查询，可以通过or()拼接sql语句
+        ComputerExample example=new ComputerExample();
+        example.createCriteria().andComputeridLike("%BIELKJA7%");
+        example.or().andComputeridLike("%BIELKJZH%");
+        //分页PageHelper
+        PageHelper.startPage(34,10);
+        List<Computer> list=mapper.selectByExample(example);
         PageInfo pageInfo=new PageInfo<>(list,5);
         for (Computer computer:list){
             System.out.println(computer);
